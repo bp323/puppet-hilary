6f5d2a5386988eaf58c4c791a8622715f71341f7
Bert Pareyn <pareyn.bert@gmail.com>
2014-10-31 12:00:42 +0000

    Add newrelic to the staging0 server

--- environments/avocet-dev/modules/localconfig/manifests/ordering/qa.pp
+++ environments/avocet-dev/modules/localconfig/manifests/ordering/qa.pp
@@ -1,7 +1,7 @@
 class localconfig::ordering::qa {
 	include localconfig::ordering
 
-	# Install the qa automation and newrelic stuff after hilary is installed
-        Class['::hilary'] -> Class['::newrelic']
-        Class['::newrelic'] -> Class['::qaautomation']
+    # Install the qa automation and newrelic stuff after hilary is installed
+    Class['::hilary'] -> Class['::newrelic']
+    Class['::newrelic'] -> Class['::qaautomation']
 }
