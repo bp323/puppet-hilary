a8fd6fa1d08c7925379a866eec0647ee94ad6ea4
Bert Pareyn <pareyn.bert@gmail.com>
2014-10-31 11:30:19 +0000

    Update app name

--- modules/newrelic/manifests/init.pp
+++ modules/newrelic/manifests/init.pp
@@ -1,5 +1,5 @@
 class newrelic (
-    $newrelic_app_name    = 'Avocet Dev0',
+    $newrelic_app_name    = 'Avocet Dev',
     $newrelic_license_key = '1e137be444b0138c591b9ecd96680c12f7311a85',
   ) {
 
